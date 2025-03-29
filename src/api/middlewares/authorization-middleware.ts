c5481fcfa3cbed8c198471d4bd8b80d64c23b065
sithija_sulochana <144917814+sithija-sulochana@users.noreply.github.com>
2025-03-29 20:39:07 +0530

    Update authorization-middleware.ts
    add annotation for authentication

--- src/api/middlewares/authorization-middleware.ts
+++ src/api/middlewares/authorization-middleware.ts
@@ -3,6 +3,7 @@ import ForbiddenError from "../../domain/errors/forbidden-error";
 import { clerkClient } from "@clerk/express";
 
 export const isAdmin = (req: Request, res: Response, next: NextFunction) => {
+    //@ts-ignore
     if (!(req?.auth?.sessionClaims?.role !== "admin")) {
         throw new ForbiddenError("Forbidden");
     }
