4d1272a1b1adfc9aabd0a8ae52ed7f35d4da44da
sithija_sulochana <144917814+sithija-sulochana@users.noreply.github.com>
2025-03-29 21:22:33 +0530

    Update index.ts
    add https

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ const app = express();
 app.use(clerkMiddleware());
 // Middleware to parse JSON data in the request body
 app.use(express.json());
-app.use(cors({origin:"aidf-horizon-frontend-sithija.netlify.app"}));
+app.use(cors({origin:"https://aidf-horizon-frontend-sithija.netlify.app/account"}));
 
 
 
